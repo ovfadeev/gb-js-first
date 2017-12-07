9d5ecb23449b3d045fe334028c4293bceabd3013
o.fadeev <o.fadeev>
2017-12-07 15:07:28 +0300

    hw1

---
+++ hw1/js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * Home work 1
+ */
+/* 1 --- convert degrees --- */
+var degreeCelcium = 24;
+if (typeof degreeCelcium === 'number'){
+	var degreeFarengeit = (9 / 5) * degreeCelcium + 32;
+	alert(degreeFarengeit);
+} else {
+	alert('Enter the number!');
+}
+/* --- end --- */
+
+/* 3 --- write name --- */
+var name = 'Test',
+	admin = name;
+alert(admin);
+/* --- end --- */
