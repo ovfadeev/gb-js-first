984eadf40a27fbfbc57ca29ed803efb09c460e58
o.fadeev <o.fadeev@itech-group.ru>
2017-12-10 21:33:23 +0300

    template

--- hw5/js/main.js
+++ hw5/js/main.js
@@ -7,25 +7,17 @@
 
 var arrLetters = ['A', 'B', 'C', 'D', 'I', 'F', 'G', 'H'],
     arrNumbers = ['1', '2', '3', '4', '5', '6', '7', '8'],
-    arrCellsClass = ['grey', 'brown'],
+    arrCellsClassBack = ['grey', 'brown'],
+    arrCellsClassText = ['white', 'black'],
     arrFiguresChess = ['', '', '', '', '', '', '', '', '', ''],
     classCells = 'cell',
     classCellsLetters = 'letters',
     classRows = 'row',
     classChess = 'chess',
-    countRowCells = 10,
+    countRowCells = 9,
     classWrapper = 'wrapper';
 
-console.log(arrLetters);
-console.log(arrNumbers);
-
 bildChessBoard();
-
-function bildRows(){
-  // bild divs cell letters
-
-  return divRow;
-}
 
 function bildChessBoard(){
   // wrapper
@@ -35,26 +27,33 @@ function bildChessBoard(){
   var divChess = document.createElement('div');
   divChess.className = classChess;
   // bild rows
-  for (var i = 0; i < countRowCells; i++) {
+  for (var i = 0; i <= countRowCells; i++) {
     // bild div row
     var divRow = document.createElement('div');
     divRow.className = classRows;
     // bild cells
-    for (var j = 0; j < countRowCells; j++) {
+    for (var j = 0; j <= countRowCells; j++) {
       var divCell = document.createElement('div');
 
-      if ((j != 0 && j != countRowCells - 1) && (i == 0 || i == countRowCells - 1)){ // row cells letters
-        divCell.className = classCells + ' ' + classCellsLetters;
+      if ((j != 0 && j != countRowCells) && (i == 0 || i == countRowCells)){ // row cells letters
+        divCell.classList.add(classCells, classCellsLetters);
         divCell.innerText = arrLetters[j-1];
-      } else if ((j == 0 || j == countRowCells - 1) && (i > 0 && i < countRowCells - 1)){ // cells number
-        divCell.className = classCells + ' ' + classCellsLetters;
+      } else if ((j == 0 || j == countRowCells) && (i > 0 && i < countRowCells)){ // cells number
+        divCell.classList.add(classCells, classCellsLetters);
         divCell.innerText = arrNumbers[i-1];
-      } else if (j != 0 && j != countRowCells - 1){ // cells collor
+      } else if (j != 0 && j != countRowCells){ // cells collor
 
         if ((j % 2 - i % 2) == 0){ // grey
-          divCell.className = classCells + ' ' + arrCellsClass[0];
+          divCell.classList.add(classCells, arrCellsClassBack[0]);
         } else { // brown
-          divCell.className = classCells + ' ' + arrCellsClass[1];
+          divCell.classList.add(classCells, arrCellsClassBack[1]);
+        }
+
+        if (i > 0 && i < 3){
+          divCell.classList.add(arrCellsClassText[0]);
+        } else if (i > 6 && i < countRowCells){
+          divCell.classList.add(arrCellsClassText[1]);
+          console.log('black');
         }
 
       } else { // cells empty
