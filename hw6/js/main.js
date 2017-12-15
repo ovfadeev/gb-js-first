eba799b897cdee7f72111f1b9fcab3097aaba676
o.fadeev <o.fadeev>
2017-12-15 11:57:42 +0300

    hw6

--- hw6/js/main.js
+++ hw6/js/main.js
@@ -4,64 +4,153 @@
  */
 
 /* --- 1 --- */
-function bigPicture(event) {
-  var picture = event.target;
-  if(picture.classList.contains(classPreviewImg)) {
-    mainView.innerHTML = '';
+/**
+ * Создание элементов
+ * @param  {[HTMLElement]} node
+ * @param  {[string]} classElement
+ * @return {[HTMLElement]}
+ */
+function createElement(node, classElement){
+  var element = document.createElement(node);
 
+  if(typeof classElement !== "undefined"){
+    element.classList.add(classElement);
+  }
+
+  return element;
+}
+/**
+ * Показываем слайд
+ * @param  {[object]} event
+ */
+function bigPicture(event) {
+  var picture = event.target,
+      mainViewImages = document.getElementById(classMainViewImages);
+  if(picture.classList.contains(classPreviewImg) && removeClassPreview()) {
+    mainViewImages.innerHTML = '';
     var bigPicture = picture.cloneNode(true);
     bigPicture.classList.remove(classPreviewImg);
     bigPicture.src = bigPicture.src.replace(previewPath, mainViewPath);
     bigPicture.classList.add(classMainViewImg);
 
-    mainView.appendChild(bigPicture);
+    mainViewImages.appendChild(bigPicture);
 
-    mainView.onerror = checkSrc;
+    mainViewImages.onerror = checkSrc;
+
+    addClassPreview(picture);
   }
   event.stopPropagation();
 }
-
-function sliderView(){
-  var viewPic = document.getElementsByClassName(classMainViewImg),
-      previewPic = document.getElementsByClassName(classPreviewImg);
-
-  gallery.addEventListener('click', bigPicture);
-
-  if (viewPic.length <= 0){
-    previewPic[0].click();
+/**
+ * Удаляем из коллекции класс
+ */
+function removeClassPreview(){
+  var previewPic = document.getElementsByClassName(classPreviewImg);
+  for (var i = 0; i < previewPic.length; i++) {
+    previewPic[i].classList.remove(classPreviewImgView);
   }
-  console.log(view);
+  return true;
 }
-
-function checkSrc(){
-  alert("Ошибка: " + this.src);
+/**
+ * Поиск активного элемента маленьких изображений
+ * @return {[HTMLElement]} эелемент
+ */
+function findActivePreview(){
+  var previewPic = document.getElementsByClassName(classPreviewImg);
+  for (var i = 0; i < previewPic.length; i++) {
+    if (previewPic[i].classList.contains(classPreviewImgView)){
+      var activePreview = previewPic[i];
+      return activePreview;
+    }
+  }
+  return false;
 }
+/**
+ * Добавить класс для элемента маленьких картинок
+ * @param {[type]} element [description]
+ */
+function addClassPreview(element){
+  return element.classList.add(classPreviewImgView);
+}
+/**
+ * Создаем галлерею
+ * @param  {[string]} galleryClass
+ * @param  {[array]} arImages
+ */
+function createPreviewImages(galleryClass, arImages){
+  var gallery = document.getElementById(galleryClass);
+  for(var i = 0; i < arImages.length; i++) {
+    var img = createElement('img', classPreviewImg);
+    img.src = previewPath + arImages[i];
 
-function init() {
-  for(var i = 0; i < pictures.length; i++) {
-    var img = document.createElement('img');
-    img.src = previewPath + pictures[i];
-    img.classList.add(classPreviewImg);
+    img.setAttribute('data-id', i);
 
     gallery.appendChild(img);
 
     img.onerror = checkSrc;
   }
 
-  sliderView();
+  gallery.addEventListener('click', bigPicture);
 
+}
+/**
+ * Переключение слайдов
+ * @param  {[object]} event
+ */
+function nextSlide(event){
+  event.preventDefault();
+  var arrow = event.target,
+      classArrowLeft = 'js-arrow-left',
+      classArrowRight = 'js-arrow-right',
+      activePreview = findActivePreview(),
+      pic = null;
+  if (arrow.classList.contains(classArrowLeft) && activePreview){
+    pic = activePreview.previousElementSibling;
+  } else if (arrow.classList.contains(classArrowRight) && activePreview){
+    pic = activePreview.nextElementSibling;
+  }
+  if (pic !== null){
+    pic.click();
+  }
+}
+/**
+ * Показ слайда
+ */
+function sliderView(){
+  var viewPic = document.getElementsByClassName(classMainViewImg),
+      previewPic = document.getElementsByClassName(classPreviewImg),
+      mainView = document.getElementById(classMainView);
+
+  mainView.addEventListener('click', nextSlide);
+
+  if (viewPic.length <= 0){
+    previewPic[0].click();
+  }
+}
+/**
+ * Проверка ошибки
+ */
+function checkSrc(){
+  alert("Ошибка: " + this.src);
+}
+/**
+ * Инициализация
+ */
+function init() {
+  createPreviewImages(classGallery, arImages);
+  sliderView();
 }
 
 var classPreviewImg = 'gallery-item',
     classPreviewImgView = 'view',
     classMainViewImg = 'view-orig-img',
     classGallery = 'gallery',
-    classMainView = 'mainView',
+    classMainView = 'main-view',
+    classMainViewImages = 'main-view-images',
+    classSliderNavigation = 'pager',
     previewPath = 'images/small/',
     mainViewPath = 'images/orig/',
-    pictures = ['1.jpg', '2.jpeg', '3.jpg'],
-    gallery = document.getElementById(classGallery),
-    mainView = document.getElementById(classMainView);
+    arImages = ['1.jpg', '2.jpeg', '3.jpg'];
 
 window.onload = init;
 /* --- end --- */
